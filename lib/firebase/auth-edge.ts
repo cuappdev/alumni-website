4873694db6dbbf1c6741087ea4efdff3916cbbfd
Daniel Li <daniel.li3397@gmail.com>
2026-03-05 10:57:43 -0500

    Error if credentials missing.

--- lib/firebase/auth-edge.ts
+++ lib/firebase/auth-edge.ts
@@ -1,3 +1,11 @@
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+if (!projectId || !clientEmail || !privateKey) {
+  throw new Error("Missing Firebase credentials");
+}
+
 export const authConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   cookieName: "__session",
@@ -12,8 +20,8 @@ export const authConfig = {
     maxAge: 60 * 60 * 24 * 14, // 14 days
   },
   serviceAccount: {
-    projectId: process.env.FIREBASE_PROJECT_ID!,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    projectId,
+    clientEmail,
+    privateKey: privateKey.replace(/\\n/g, "\n"),
   },
 };
