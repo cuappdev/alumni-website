897d2e593cc22e344a94fa35df2e4df35700aba8
Daniel Li <daniel.li3397@gmail.com>
2026-03-05 10:51:37 -0500

    Attempt:

--- lib/firebase/auth-edge.ts
+++ lib/firebase/auth-edge.ts
@@ -14,6 +14,6 @@ export const authConfig = {
   serviceAccount: {
     projectId: process.env.FIREBASE_PROJECT_ID!,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY!,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
   },
 };
